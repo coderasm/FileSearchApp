cc423c3ca747f3e862479cbbb7e9f23fdc814f80
pkrueger <philip.krueger@caci.com>
2019-05-01 10:00:18 -0700

    first push

--- StringSearchApp/Form1.Designer.cs
+++ StringSearchApp/Form1.Designer.cs
@@ -28,13 +28,143 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
+      this.button1 = new System.Windows.Forms.Button();
+      this.filePath = new System.Windows.Forms.Label();
+      this.searchTextBox = new System.Windows.Forms.TextBox();
+      this.searchTextLabel = new System.Windows.Forms.Label();
+      this.searchButton = new System.Windows.Forms.Button();
+      this.resultLabel = new System.Windows.Forms.Label();
+      this.resultsCount = new System.Windows.Forms.Label();
+      this.resultList = new System.Windows.Forms.ListBox();
+      this.previousButton = new System.Windows.Forms.Button();
+      this.nextButton = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(12, 12);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(67, 22);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Load";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.loadButtonClicked);
+      // 
+      // filePath
+      // 
+      this.filePath.AutoSize = true;
+      this.filePath.Location = new System.Drawing.Point(85, 17);
+      this.filePath.Name = "filePath";
+      this.filePath.Size = new System.Drawing.Size(82, 13);
+      this.filePath.TabIndex = 1;
+      this.filePath.Text = "Select a text file";
+      // 
+      // searchTextBox
+      // 
+      this.searchTextBox.Location = new System.Drawing.Point(12, 55);
+      this.searchTextBox.Name = "searchTextBox";
+      this.searchTextBox.Size = new System.Drawing.Size(119, 20);
+      this.searchTextBox.TabIndex = 2;
+      // 
+      // searchTextLabel
+      // 
+      this.searchTextLabel.AutoSize = true;
+      this.searchTextLabel.Location = new System.Drawing.Point(12, 39);
+      this.searchTextLabel.Name = "searchTextLabel";
+      this.searchTextLabel.Size = new System.Drawing.Size(65, 13);
+      this.searchTextLabel.TabIndex = 3;
+      this.searchTextLabel.Text = "Search Text";
+      // 
+      // searchButton
+      // 
+      this.searchButton.Location = new System.Drawing.Point(12, 81);
+      this.searchButton.Name = "searchButton";
+      this.searchButton.Size = new System.Drawing.Size(75, 23);
+      this.searchButton.TabIndex = 4;
+      this.searchButton.Text = "Search";
+      this.searchButton.UseVisualStyleBackColor = true;
+      this.searchButton.Click += new System.EventHandler(this.searchButtonClicked);
+      // 
+      // resultLabel
+      // 
+      this.resultLabel.AutoSize = true;
+      this.resultLabel.Location = new System.Drawing.Point(549, 17);
+      this.resultLabel.Name = "resultLabel";
+      this.resultLabel.Size = new System.Drawing.Size(45, 13);
+      this.resultLabel.TabIndex = 5;
+      this.resultLabel.Text = "Results:";
+      // 
+      // resultsCount
+      // 
+      this.resultsCount.AutoSize = true;
+      this.resultsCount.Location = new System.Drawing.Point(589, 17);
+      this.resultsCount.Name = "resultsCount";
+      this.resultsCount.Size = new System.Drawing.Size(13, 13);
+      this.resultsCount.TabIndex = 6;
+      this.resultsCount.Text = "0";
+      // 
+      // resultList
+      // 
+      this.resultList.FormattingEnabled = true;
+      this.resultList.Location = new System.Drawing.Point(157, 39);
+      this.resultList.Name = "resultList";
+      this.resultList.Size = new System.Drawing.Size(852, 589);
+      this.resultList.TabIndex = 7;
+      // 
+      // previousButton
+      // 
+      this.previousButton.Location = new System.Drawing.Point(12, 110);
+      this.previousButton.Name = "previousButton";
+      this.previousButton.Size = new System.Drawing.Size(75, 23);
+      this.previousButton.TabIndex = 8;
+      this.previousButton.Text = "Previous";
+      this.previousButton.UseVisualStyleBackColor = true;
+      this.previousButton.Click += new System.EventHandler(this.previousClick);
+      // 
+      // nextButton
+      // 
+      this.nextButton.Location = new System.Drawing.Point(12, 139);
+      this.nextButton.Name = "nextButton";
+      this.nextButton.Size = new System.Drawing.Size(75, 23);
+      this.nextButton.TabIndex = 9;
+      this.nextButton.Text = "Next";
+      this.nextButton.UseVisualStyleBackColor = true;
+      this.nextButton.Click += new System.EventHandler(this.nextClick);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.ClientSize = new System.Drawing.Size(1021, 641);
+      this.Controls.Add(this.nextButton);
+      this.Controls.Add(this.previousButton);
+      this.Controls.Add(this.resultList);
+      this.Controls.Add(this.resultsCount);
+      this.Controls.Add(this.resultLabel);
+      this.Controls.Add(this.searchButton);
+      this.Controls.Add(this.searchTextLabel);
+      this.Controls.Add(this.searchTextBox);
+      this.Controls.Add(this.filePath);
+      this.Controls.Add(this.button1);
+      this.Name = "Form1";
       this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
     }
 
     #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label filePath;
+    private System.Windows.Forms.TextBox searchTextBox;
+    private System.Windows.Forms.Label searchTextLabel;
+    private System.Windows.Forms.Button searchButton;
+    private System.Windows.Forms.Label resultLabel;
+    private System.Windows.Forms.Label resultsCount;
+    private System.Windows.Forms.ListBox resultList;
+    private System.Windows.Forms.Button previousButton;
+    private System.Windows.Forms.Button nextButton;
   }
 }
 
--- StringSearchApp/Form1.cs
+++ StringSearchApp/Form1.cs
@@ -3,18 +3,91 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace StringSearchApp
 {
   public partial class Form1 : Form
   {
+    private string[] fileLines;
+    private int highlightIndex = 0;
+    private OpenFileDialog fileDialog = new OpenFileDialog();
     public Form1()
     {
       InitializeComponent();
+      fileDialog.Multiselect = false;
+    }
+
+    private void loadButtonClicked(object sender, EventArgs e)
+    {
+      if (fileDialog.ShowDialog() == DialogResult.OK)
+      {
+        try
+        {
+          filePath.Text = fileDialog.FileName;
+          var sr = new StreamReader(fileDialog.FileName);
+          fileLines = sr.ReadToEnd().Split('\n');
+        }
+        catch (SecurityException ex)
+        {
+          MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+          $"Details:\n\n{ex.StackTrace}");
+        }
+      }
+    }
+
+    private void searchButtonClicked(object sender, EventArgs e)
+    {
+      if (searchTextBox.Text != "" && fileLines.Length > 0)
+      {
+        resultList.Items.Clear();
+        var regex = new Regex($@"(^|\s){searchTextBox.Text}(\s|$)", RegexOptions.IgnoreCase);
+        for (int i = 0; i < fileLines.Length; i++)
+        {
+          var matches = regex.Matches(fileLines[i]);
+          if (matches.Count > 0)
+          {
+            foreach (var match in matches)
+            {
+              var entry = $"Line {i + 1}: Word: \"{match.ToString()}\", sentence: {fileLines[i]}";
+              resultList.Items.Add(entry);
+            }
+          }
+        }
+        resultsCount.Text = resultList.Items.Count.ToString();
+        resultList.SetSelected(0, true);
+      }
+    }
+
+    private void previousClick(object sender, EventArgs e)
+    {
+      if (resultList.Items.Count > 0)
+      {
+        resultList.SetSelected(highlightIndex, false);
+        if (highlightIndex == 0)
+          highlightIndex = resultList.Items.Count - 1;
+        else
+          --highlightIndex;
+        resultList.SetSelected(highlightIndex, true);
+      }
+    }
+
+    private void nextClick(object sender, EventArgs e)
+    {
+      if (resultList.Items.Count > 0)
+      {
+        resultList.SetSelected(highlightIndex, false);
+        if (highlightIndex == resultList.Items.Count - 1)
+          highlightIndex = 0;
+        else
+          ++highlightIndex;
+        resultList.SetSelected(highlightIndex, true);
+      }
     }
   }
 }
